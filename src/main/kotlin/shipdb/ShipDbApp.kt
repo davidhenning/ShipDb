c6a328fc01edb039b4c7e997b2148fd3bfa8baef
David Henning <david@webcodr.de>
2017-10-03 23:37:33 +0200

    Add API route to get a ship by its id

--- src/main/kotlin/shipdb/ShipDbApp.kt
+++ src/main/kotlin/shipdb/ShipDbApp.kt
@@ -4,7 +4,10 @@ import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.features.CallLogging
 import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.gson.GsonSupport
 import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.http.HttpStatusCode
+import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
@@ -12,6 +15,15 @@ import org.jetbrains.ktor.routing.get
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(GsonSupport) {
+        setPrettyPrinting()
+    }
+
+    val ships = listOf(
+            Ship(1, "Enterprise", "NCC-1701-E"),
+            Ship(2, "Defiant", "NX-74205")
+    )
+
     install(Routing) {
         get("/") {
             call.respondText("<h1>Welcome to ShipDB</h1>", ContentType.Text.Html)
@@ -19,6 +31,18 @@ fun Application.main() {
 
         get("/api") {
             call.respondText("{}", ContentType.Application.Json)
+        }
+
+        get("/api/v1/ship/{id}") {
+            val item = ships.firstOrNull { it.id == call.parameters["id"]?.toInt() }
+
+            if (item == null) {
+                val error = ErrorResponse("Ship not found")
+                call.response.status(HttpStatusCode.NotFound)
+                call.respond(error)
+            } else {
+                call.respond(item)
+            }
         }
     }
 }
